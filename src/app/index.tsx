45bba8921a4f31911d195c9202fc4f7187ad89ee
chieduonye <chieduchineme@gmail.com>
2023-09-19 16:16:05 +0100

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import Canvas from 'app/components/canvas'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header-info">
+        <h1 className="app-title">React Sine Wave</h1>
+        <p className="project-source">
+        </p>
+      </div>
+
+      <Canvas />
+    </div>
+  )
+}
+
+export default App
